4921be9aa29e097fc3da2a8c79f2b1d410e8de11
Cloud Bai <46363139+cloudybai@users.noreply.github.com>
2023-10-23 23:27:20 +0100

    Add files via upload

---
+++ experiments/RSUNet/test.sh
@@ -0,0 +1,4 @@
+
+export PYTHONPATH=/home/bbd/Desktop/remove_cloud/
+python /home/bbd/Desktop/remove_cloud/main/test.py  \
+--config_file='/home/yub3/cloud/remove_cloud_codes/experiments/RSUNet/config.yaml'
