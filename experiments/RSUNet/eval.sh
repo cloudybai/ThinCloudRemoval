ce03f76eebf77536f8f3ce53772fdd211d0176e2
Cloud Bai <46363139+cloudybai@users.noreply.github.com>
2023-10-23 23:28:26 +0100

    Delete experiments/RSUNet directory

--- experiments/RSUNet/eval.sh
+++
@@ -1,3 +0,0 @@
-export PYTHONPATH=/home/data1/xiesong/git_repo/removal_thin_cloud/
-python /home/data1/xiesong/git_repo/removal_thin_cloud/main/select_model.py  \
---config_file='/home/data1/xiesong/git_repo/removal_thin_cloud/experiments/UNet/config.yaml'
--- experiments/RSUNet/test.sh
+++
@@ -1,4 +0,0 @@
-
-export PYTHONPATH=/home/bbd/Desktop/remove_cloud/
-python /home/bbd/Desktop/remove_cloud/main/test.py  \
---config_file='/home/yub3/cloud/remove_cloud_codes/experiments/RSUNet/config.yaml'
