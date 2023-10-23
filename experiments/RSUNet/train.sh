17379725f264f6e8aeaee5a4f1acdb5d0e8c9f3d
Cloud Bai <46363139+cloudybai@users.noreply.github.com>
2023-10-23 23:29:03 +0100

    Add files via upload

---
+++ experiments/RSUNet/train.sh
@@ -0,0 +1,9 @@
+#export PYTHONPATH=/home/xqzhou/xscode/remove_cloud/
+#python /home/xqzhou/xscode/remove_cloud/main/train.py  \
+#--config_file='/home/xqzhou/xscode/remove_cloud/experiments/UNet/config.yaml'
+
+#export PYTHONPATH=/home/bbd/Desktop/remove_cloud/
+
+
+python /home/yub3/cloud/remove_cloud_codes/main/train.py  \
+--config_file='//home/yub3/cloud/remove_cloud_codes/experiments/RSUNet/config.yaml'
